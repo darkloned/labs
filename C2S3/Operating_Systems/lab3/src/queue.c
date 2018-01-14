e6bef42b2ce6aa53e0e0b962d5c6af9bb8371e91
HagRead-Only <migalev-roman@ya.ru>
2018-01-14 21:15:59 +0300

    lab3 updated

--- C2S3/Operating_Systems/lab3/src/queue.c
+++ C2S3/Operating_Systems/lab3/src/queue.c
@@ -18,25 +18,39 @@ bool isEmpty(Queue* q)
 	return q->size == 0;
 }
 
-// not failsafe functions below
-
 void push(Queue* q, int v)
 {
-	q->back = isEmpty(q) ? q->front : (q->back + 1) % ARRAYSIZE(q->pool);
-	q->pool[q->back] = v;
+	if (q->size == sizeof(q->pool) / sizeof(int))
+	{
+		printf("Push (%d) declined, queue is full.\n", v);
+	}
+	else
+	{
+		q->back = isEmpty(q) ? q->front : (q->back + 1) % ARRAYSIZE(q->pool);
+		q->pool[q->back] = v;
 
-	q->size++;
+		q->size++;
+	}
 }
 
 int pop(Queue* q)
 {
-	int v = q->pool[q->front];
+	if (q->size == 0)
+	{
+		printf("Pop declined, queue is empty. Returning zero-value.\n");
 
-	q->front = (q->front + 1) % ARRAYSIZE(q->pool);
+		return 0;
+	}
+	else
+	{
+		int v = q->pool[q->front];
 
-	q->size--;
+		q->front = (q->front + 1) % ARRAYSIZE(q->pool);
 
-	return v;
+		q->size--;
+
+		return v;
+	}
 }
 
 void deleteQueue(Queue* q)
